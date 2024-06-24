65b78a39387f0fb5f6d35d2c84119fc687f71163
Axel Clark <axel@viabeacon.com>
2024-06-24 08:54:20 -0700

    Add Code.ensure_loaded before checking if function exported

--- lib/mimic.ex
+++ lib/mimic.ex
@@ -465,8 +465,15 @@ defmodule Mimic do
   end
 
   defp raise_if_not_exported_function!(module, fn_name, arity) do
-    unless function_exported?(module, fn_name, arity) do
-      raise ArgumentError, "Function #{fn_name}/#{arity} not defined for #{inspect(module)}"
+    case Code.ensure_loaded(module) do
+      {:module, _module} ->
+        unless function_exported?(module, fn_name, arity) do
+          raise ArgumentError, "Function #{fn_name}/#{arity} not defined for #{inspect(module)}"
+        end
+
+      {:error, reason}
+      when reason in [:embedded, :badfile, :nofile, :on_load_failure, :unavailable] ->
+        raise ArgumentError, "Module #{inspect(module)} is not available"
     end
   end
 
